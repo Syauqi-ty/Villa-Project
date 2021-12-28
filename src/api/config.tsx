e1ada1676d5ddef0444b638d8877526efd323114
Syauqi-ty <55091752+Syauqi-ty@users.noreply.github.com>
2021-12-28 14:56:46 +0700

    Add files via upload

---
+++ src/api/config.tsx
@@ -0,0 +1,6 @@
+export const Url = "http://localhost:5000/api";
+
+export const Header = {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+};
---
+++ src/api/exam.tsx
@@ -0,0 +1,23 @@
+export const DataMonth = [
+  {
+    name: "Januari",
+    Income: 0,
+    Expense: 0,
+  },
+];
+
+export const DataWeek = [
+  {
+    name: "Week-1",
+    Income: 0,
+    Expense: 0,
+  },
+];
+
+export const DataYear = [
+  {
+    name: String(new Date().getFullYear()),
+    Income: 0,
+    Expense: 0,
+  },
+];
